fcf216019bbd7cfdb669f551aed9b289d459c3ae
nisalmissaka <wishwakanisal@gmail.com>
2026-05-20 17:50:48 +0530

    Add Django authentication signup and signin pages

--- myproject/authentication/views.py
+++ myproject/authentication/views.py
@@ -1,11 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 def home(request):
     return render(request, "authentication/index.html")
 
 
 def signup(request):
+
+    if request.method == "POST":
+       # username = request.POST.get('username')
+        username = request.POST['username']
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        email = request.POST['email']
+        password = request.POST['password']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create_user(username,email,password)
+        myuser.first_name = firstname
+        myuser.last_name = lastname
+
+        myuser.save()
+
+        messages.success(request, "Your Account has been successfully created.")
+
+        return redirect('signin')
+
+
     return render(request, "authentication/signup.html")
 
 
